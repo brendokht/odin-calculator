56362b74ac1e11ab09727d9550d38ad121ad7748
brendokht <103231108+brendokht@users.noreply.github.com>
2023-08-23 20:34:51 -0400

    Set up initial files and added operator functions

---
+++ index.js
@@ -0,0 +1,38 @@
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(num1, num2, op) {
+    switch (op) {
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            subtract(num1, num2);
+            break;
+        case "*":
+            multiply(num1, num2);
+            break;
+        case "/":
+            divide(num1, num2);
+            break;
+        default:
+            throw new Error("Invalid operator");
+    }
+}
